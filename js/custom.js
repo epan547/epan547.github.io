5f4a1d4c49d331bbf7542fb20f41ee6308ecec1d
epan547 <epan547@gmail.com>
2021-01-10 14:44:55 -0500

    Updating projects section with some recent developments

--- js/custom.js
+++ js/custom.js
@@ -43,38 +43,6 @@ $(document).ready(function() {
     });
 
 
- /* Home Slideshow Vegas
-  -----------------------------------------------*/
-  $(function() {
-    $('body').vegas({
-        slides: [
-            { src: 'images/slide-img1.jpg' },
-            { src: 'images/slide-img2.jpg' },
-            { src: 'images/slide-img3.jpg' }
-        ],
-        timer: false,
-        transition: [ 'zoomIn', ],
-        animation: ['kenburns']
-    });
-  });
-
-
-  /* Team carousel
-  -----------------------------------------------*/
-  $(document).ready(function() {
-      $("#team-carousel").owlCarousel({
-          items : 3,
-          itemsDesktop : [1199,3],
-          itemsDesktopSmall : [979,3],
-          slideSpeed: 300,
-          itemsDesktop : [1199,2],
-          itemsTablet: [768,1],
-          itemsTabletSmall: [985,2],
-          itemsMobile : [479,1],
-      });
-    });
-
-
     /* Back to Top
     -----------------------------------------------*/
     $(window).scroll(function() {
